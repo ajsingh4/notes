363138de823cd67f05219fac2beb30299f70864e
Aj Gill <ajgill@ucdavis.edu>
2019-07-28 14:55:22 -0700

    Note Removal & Removal Testing
    Added functionality to delete a note from the DB by the ID of the note but the user must be logged in. Verified this via tests in ensuring that notes not associated with that user can not be deleted.

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -1,13 +1,14 @@
 import {Mongo} from "meteor/mongo";
 import {Meteor} from "meteor/meteor";
 import moment from "moment";
+import SimpleSchema from "simpl-schema";
 
 export const Notes = new Mongo.Collection("notes");
 
 Meteor.methods({
   "notes.insert"() {
     if(!this.userId) {
-      throw new Meteor.Error("Not Authorized")
+      throw new Meteor.Error("Not Authorized");
     }
 
     return Notes.insert({
@@ -15,6 +16,18 @@ Meteor.methods({
       body: "",
       userId: this.userId,
       updatedAt: moment().valueOf()
-    });
+    })
+  },
+    "notes.remove"(_id) {
+      if(!this.userId){
+        throw new Meteor.Error("Not Authorized");
+      }
+      new SimpleSchema({
+        _id: {
+          type: String,
+          min: 1
+        }
+      }).validate({_id});
+      Notes.remove({_id, userId: this.userId});
   }
 });
