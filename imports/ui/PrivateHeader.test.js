34c336371f8f4901bf07c1c1358a59f2bf96fa42
Aj Gill <ajgill@ucdavis.edu>
2019-08-07 19:09:38 -0700

    Function testing via spies

--- imports/ui/PrivateHeader.test.js
+++ imports/ui/PrivateHeader.test.js
@@ -12,16 +12,29 @@ configure({ adapter: new Adapter() });
 if(Meteor.isClient) {
   describe("PrivateHeader", function() {
     it("should set button text to logout", function() {
-      const wrapper = mount(<PrivateHeader title="Some title"/>);
+      const wrapper = mount(<PrivateHeader title="Some title" handleLogout={() => {}}/>);
       const buttonText = wrapper.find("button").text();
       expect(buttonText).toBe("Logout");
     });
 
     it("should use title prop as h1 text", function() {
       const title = "Test title here";
-      const wrapper = mount(<PrivateHeader title={title}/>);
+      const wrapper = mount(<PrivateHeader title={title} handleLogout={() => {}}/>);
       const h1Text = wrapper.find("h1").text();
       expect(h1Text).toBe(title)
+    });
+
+    // it("should call the function", function() {
+    //   const spy = expect.createSpy();
+    //   spy(3, 4, 123);
+    //   expect(spy).toHaveBeenCalledWith(3);
+    // });
+
+    it("should call handlelogout on click", function() {
+      const spy = expect.createSpy();
+      const wrapper = mount(<PrivateHeader title="Title" handleLogout={spy}/>);
+      wrapper.find("button").simulate("click");
+      expect(spy).toHaveBeenCalled();
     });
   });
 }
