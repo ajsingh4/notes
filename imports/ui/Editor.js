5d362d95c1d995f959f3f1e1f61ca621ed212446
Aj Gill <ajgill@ucdavis.edu>
2019-09-05 20:08:30 -0700

    Update Editor.js
    Can change title and body via editor now

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -3,14 +3,31 @@ import {createContainer} from "meteor/react-meteor-data";
 import {Session} from "meteor/session";
 import {Notes} from "../api/notes";
 import PropTypes from 'prop-types';
-//Going to use create createContainer
+import {Meteor} from "meteor/meteor";
+
 //Use session.get to figure out which session is selected and used to see if note exists in the db
 export class Editor extends React.Component {
+  handleBodyChange(e) {
+    //Will be able to access the dom element on the e location -> the area value
+    this.props.call("notes.update", this.props.note._id, {
+      //e.target gets us to the dom and then access the value
+      body: e.target.value
+    })
+  };
+  handleTitleChange(e) {
+    this.props.call("notes.update", this.props.note._id, {
+      title: e.target.value
+    })
+  }
   render() {
     //3 possibilities, get a note, get an ID but not a match, get nothing
     if(this.props.note) {
       return (
-        <p>We got the note</p>
+        <div>
+          <input value = {this.props.note.title} placeholder="Your title" onChange={this.handleTitleChange.bind(this)}/>
+          <textarea value = {this.props.note.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
+          <button>Delete Note</button>
+        </div>
       );
     }
     else {
@@ -26,7 +43,8 @@ export default createContainer(() => {
   const selectedNoteId = Session.get("selectedNoteId");
   return {
     selectedNoteId,
-    note: Notes.findOne(selectedNoteId)
+    note: Notes.findOne(selectedNoteId),
+    call: Meteor.call
   };
 }, Editor);
 
